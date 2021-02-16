943d5b8b7983e1a3b86aa179fe8b46000a44e474
mohamed <im_gouaouri@esidz>
2021-02-16 14:23:40 +0100

    add timeleft command

--- cogs/hashcode.py
+++ cogs/hashcode.py
@@ -27,26 +27,12 @@ class hashcode(commands.Cog):
     @commands.command(description="Get the time left to the end of the competition")
     async def timeLeft(self, ctx):
         now = datetime.datetime.now()
-        delta = end - now
+        delta = self.end - now
         hours_left = delta.seconds // 3600
         minutes_left = (delta.seconds % 3600) // 60
         seconds_left = (delta.seconds % 3600 ) % 60
-        await ctx.send(f"Time left {hours_left}hours {minutes_left}minutes {seconds_left}seconds")
+        await ctx.message.channel.send(f"Time left {hours_left}hours {minutes_left}minutes {seconds_left}seconds")
     
-    # post a message by the moderator to a specified channel
-    @commands.command(description="Post in a specified channel (Only bot moderator)")
-    async def post(ctx, channel_name, *, message=None):
-        author = ctx.message.author
-        if "botmoderator" in [y.name.lower() for y in author.roles]:
-            channel_obj = discord.utils.get(ctx.guild.channels, name=channel_name)
-            if channel_obj:
-                channel_id = channel_obj.id
-                channel = bot.get_channel(channel_id)
-                await channel.send(message)
-            else:
-                await ctx.send(f":weary: channel {channel_name} does not exist")
-
-
 
 
 def setup(bot):
