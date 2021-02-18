2cff8d84ca13516d155609731662bfca74e01017
akram09 <a.boutouchent@esi-sba.dz>
2021-02-18 22:58:01 +0100

    Add hashcode cog

--- cogs/hashcode.py
+++ cogs/hashcode.py
@@ -9,3 +9,13 @@ if not os.path.isfile("config.py"):
     sys.exit("'config.py' not found! Please add it and try again.")
 else:
     import config
+
+
+class hashcode(commands.Cog, name="hashcode"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="test")
+    async def test(self, context):
+        "This is a test command"
+        await context.send("This is a test command")
