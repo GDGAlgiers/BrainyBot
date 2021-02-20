e1195297bd0b0f567a7e75c02a55186637347c67
nabildroid <pni20156789@gmail.com>
2021-02-20 17:17:13 +0000

    invite works only in team_category

--- cogs/invite.py
+++ cogs/invite.py
@@ -8,38 +8,39 @@ else:
     import config
 
 
+def Auth(uuid):
+    # todo implement the auth function
+    return True, "test"
+
+
 class Invite(commands.Cog, name="invite"):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="invite")
     async def invite(self, context, member: discord.Member):
+
         author = context.author
         currentChannel = context.channel
         currentCategory = currentChannel.category
 
-        if (currentCategory.name == config.CATEGORY_FOR_INVITATION):
-            categories = author.guild.categories
-            team_categories = filter(
-                lambda category: category.name.startswith(
-                    config.TEAM_WORKSPACE_PREFIX) == True and category.
-                permissions_for(author).read_messages == True, categories)
+        exists, teamName = Auth(author.id)
 
-            team_category = next(team_categories)
+        if (exists and currentCategory.name == f"{teamName}_category"):
+
+            team_category = currentCategory
             #team_channels = team_category.channels
 
-            overwrite = discord.PermissionOverwrite(
-                view_channel=True,
-                read_messages=True,
-                read_message_history=True,
-                send_messages=True,
-                embed_links=True,
-                attach_files=True,
-                add_reactions=True,
-                connect=True,
-                speak=True,
-                stream=True
-            )
+            overwrite = discord.PermissionOverwrite(view_channel=True,
+                                                    read_messages=True,
+                                                    read_message_history=True,
+                                                    send_messages=True,
+                                                    embed_links=True,
+                                                    attach_files=True,
+                                                    add_reactions=True,
+                                                    connect=True,
+                                                    speak=True,
+                                                    stream=True)
 
             await team_category.set_permissions(member, overwrite=overwrite)
 
