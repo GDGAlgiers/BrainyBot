85484c2b2584bb735cfb6ca05d90267a49f19b2a
akram09 <a.boutouchent@esi-sba.dz>
2021-02-23 10:40:24 +0100

    Finish invite command

--- cogs/hashcode.py
+++ cogs/hashcode.py
@@ -95,6 +95,7 @@ class hashcode(commands.Cog, name="hashcode"):
             participant = {}
             user_id = ctx.author.id
             cancelled = False
+            await ctx.author.trigger_typing()
             try:
                 sent_initial_message = await ctx.author.send("Hello To checkin fonctionality please provide us your key ")
                 response = await self.bot.wait_for('message',timeout=120, check=message_check(channel=ctx.author.dm_channel))
@@ -106,49 +107,55 @@ class hashcode(commands.Cog, name="hashcode"):
                 await sent_initial_message.delete()
             if not cancelled:
                 uuid  = response.content.strip()
+                await ctx.author.trigger_typing()
                 if len(uuid) != UUID_LENGTH or  not re.match(r'[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}',uuid) :
                     await ctx.author.send(embed=Error("Wrong UUID !"))
                 else:
                     TeamExist,TeamName=Auth(uuid)
-                    #retrieving the Server guild
-                    guild = self.bot.guilds[0]                  
-                    workspace_category = guild.get_channel(config.HASHCODE_CATEGORY_WORKSPACE_ID)
-                    team_role = get(guild.roles, name=f"HASHCODE_{TeamName}_member")
-                    if team_role:
-                        if team_role in ctx.author.roles:
-                            await ctx.author.send(embed=Error("You have already joined your space"))
+                    TeamName = TeamName.lower()
+                    if TeamExist:
+                        #retrieving the Server guild
+                        guild = self.bot.guilds[0]                  
+                        workspace_category = guild.get_channel(config.HASHCODE_CATEGORY_WORKSPACE_ID)
+                        team_role = get(guild.roles, name=f"hashcode_{TeamName}_member")
+                        if team_role:
+                            if team_role in ctx.author.roles:
+                                await ctx.author.send(embed=Error("You have already joined your space"))
+                            else:
+                                await ctx.author.add_roles(team_role)
+                                await ctx.author.send(":white_check_mark: WELCOME TO GDG ALGIERS HUB")
+                                #sending logs to logs channel 
+                                log_channel = guild.get_channel(config.HASHCODE_LOGS_CHANNEL_ID)
+                                await log_channel.send(f" {ctx.author.mention} has joined {TeamName}_team .")
+                                users[user_id]={
+                                    "lastactive" : time.time()
+                                }
                         else:
-                            await ctx.author.add_roles(team_role)
-                            await ctx.author.send(":white_check_mark: WELCOME TO GDG ALGIERS HUB")
-                            #sending logs to logs channel 
-                            log_channel = guild.get_channel(config.HASHCODE_LOGS_CHANNEL_ID)
-                            await log_channel.send(f" {ctx.author.mention} has joined {TeamName}_team .")
-                            users[user_id]={
-                                "lastactive" : time.time()
+                            await guild.create_role(name=f"hashcode_{TeamName}_member")
+                            time.sleep(1)
+                            role = get(guild.roles, name=f"hashcode_{TeamName}_member")
+                            overwrites = {
+                                guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                                guild.me: discord.PermissionOverwrite(read_messages=True),
+                                role: discord.PermissionOverwrite(read_messages=True)
                             }
+                            try:
+                                channel = await guild.create_text_channel(f"hashcode_{TeamName}_space",category=workspace_category, overwrites=overwrites)
+                                channel_voice = await guild.create_voice_channel(f"hashcode_{TeamName}_space",category=workspace_category, overwrites=overwrites)
+                            except Exception as errors:
+                                print(f"Bot Error: {errors}")
+                            finally:
+                                await ctx.author.add_roles(role)
+                                await ctx.author.send(":white_check_mark: WELCOME TO GDG ALGIERS HUB")
+                                #sending logs to logs channel 
+                                log_channel = guild.get_channel(config.HASHCODE_LOGS_CHANNEL_ID)
+                                await log_channel.send(f" {ctx.author.mention} has joined {TeamName}_team .")
+                                users[user_id]={
+                                    "lastactive" : time.time()
+                                }
                     else:
-                        await guild.create_role(name=f"HASHCODE_{TeamName}_member")
-                        time.sleep(1)
-                        role = get(guild.roles, name=f"HASHCODE_{TeamName}_member")
-                        overwrites = {
-                            guild.default_role: discord.PermissionOverwrite(read_messages=False),
-                            guild.me: discord.PermissionOverwrite(read_messages=True),
-                            role: discord.PermissionOverwrite(read_messages=True)
-                        }
-                        try:
-                            channel = await guild.create_text_channel(f"HASHCODE_{TeamName}_space",category=workspace_category, overwrites=overwrites)
-                            channel_voice = await guild.create_voice_channel(f"HASHCODE_{TeamName}_space",category=workspace_category, overwrites=overwrites)
-                        except Exception as errors:
-                            print(f"Bot Error: {errors}")
-                        finally:
-                            await ctx.author.add_roles(role)
-                            await ctx.author.send(":white_check_mark: WELCOME TO GDG ALGIERS HUB")
-                            #sending logs to logs channel 
-                            log_channel = guild.get_channel(config.HASHCODE_LOGS_CHANNEL_ID)
-                            await log_channel.send(f" {ctx.author.mention} has joined {TeamName}_team .")
-                            users[user_id]={
-                                "lastactive" : time.time()
-                            }
+                        await ctx.author.send(embed=Error("Wrong UUID !"))
+                    
                 
     @commands.command(name="clear")
     async def clear(self, ctx):
@@ -160,6 +167,53 @@ class hashcode(commands.Cog, name="hashcode"):
             for channel in channels:
                 await channel.delete()
             await ctx.send("All the channels had been deleted ")
+    
+    @commands.command(name="invite")
+    async def invite(self, ctx, member: discord.Member):
+        """   Invite teammate to the workspace !!"""
+        author = ctx.author
+        currentChannel = ctx.channel
+        currentCategory = currentChannel.category
+        authorRole = ctx.author.roles 
+        team_name =re.findall(r'^hashcode_(.*)_space$',currentChannel.name)
+        guild = self.bot.guilds[0]    
+        if currentCategory.id==config.HASHCODE_CATEGORY_WORKSPACE_ID and re.match(r'^hashcode_.*_space$', currentChannel.name) and team_name !=None and len(team_name)==1 and get(guild.roles, name=f"hashcode_{team_name[0]}_member"):
+            user_id = ctx.author.id
+            cancelled = False
+            await ctx.author.trigger_typing()
+            try:
+                sent_initial_message = await ctx.author.send("To invite members we need the key of your team")
+                response = await self.bot.wait_for('message',timeout=120, check=message_check(channel=ctx.author.dm_channel))
+            except asyncio.TimeoutError: 
+                await ctx.author.send("you took too long to provide the requested information.")
+                cancelled = True
+            finally:
+                await sent_initial_message.delete()
+                response.delete()
+            if not cancelled:
+                uuid  = response.content.strip()
+                await ctx.author.trigger_typing()
+                if len(uuid) != UUID_LENGTH or  not re.match(r'[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}',uuid) :
+                    await ctx.author.send(embed=Error("Wrong UUID !"))
+                else:
+                    TeamExist,TeamName=Auth(uuid)
+                    TeamName = TeamName.lower()
+                    if TeamName!=team_name:
+                        await ctx.author.send(embed=Error("You provided a wrong uuid  !"))
+                    else:
+                        if TeamExist:
+                            team_role = get(guild.roles, name=f"hashcode_{TeamName}_member")
+                            if team_role:
+                                if team_role in ctx.author.roles:
+                                    await ctx.author.send(embed=Error("The member has already joined the workspace"))
+                                else:
+                                    await member.add_roles(team_role)
+                                    await member.send(":white_check_mark: WELCOME TO GDG ALGIERS HUB")
+                                    await ctx.author.send(":white_check_mark: MEMBER INVITED !")
+                            else:
+                                await ctx.author.send(embed=Error("Something wrong happened seems like no one has created the workspace please use `!checkin` to create workspace"))
+                        else:
+                            await ctx.author.send(embed=Error("Wrong UUID !"))
 
 
 
